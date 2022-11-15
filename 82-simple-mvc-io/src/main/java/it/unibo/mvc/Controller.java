90857abd8c5e6e66e582aa83df2ec89b65837fc9
Mauro Pellonara <mauro.pellonara@studio.unibo.it>
2022-11-15 17:26:05 +0100

    Improve code

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -4,54 +4,33 @@ import java.io.File;
 import java.io.IOException;
 import java.io.PrintStream;
 
-/**
- *
- */
 public class Controller {
 
     private File currentFile = new File(System.getProperty("user.home") + 
                                         System.getProperty("file.separator") + 
                                         "output.txt");
 
-    /**
-     * @return the current file
-     */
     public File getCurrentFile() {
         return currentFile;
     }
 
-    /**
-     * @return the current file path
-     */
     public String getCurrentFilePath() {
         return currentFile.getPath();
     }
 
-    /**
-     * Saves some text on the designed file.
-     * 
-     * @param text
-     *            the text to save
-     * @throws IOException
-     *             if the writing fails
-     */
     public void save(final String text) throws IOException {
         try (PrintStream ps = new PrintStream(currentFile)) {
             ps.println(text);
         }
     }
 
-    /**
-     * @param file
-     *            the file where to write
-     */
     public void setCurrentFile(final File file) {
         // we check that the file has a parent so that we know that the file path is valid
         final File parent = file.getParentFile();
         if (parent.exists()) {
-            currentFile = file;
+            this.currentFile = file;
         } else {
-            throw new IllegalArgumentException("Cannot save in a non-existing folder.");
+            throw new IllegalArgumentException("File path is not valid");
         }
     }
 }
