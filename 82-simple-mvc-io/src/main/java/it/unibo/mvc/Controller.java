b2a89aa0f9195f86192ea4fe45434c6ef9f804ae
Mauro Pellonara <mauro.pellonara@studio.unibo.it>
2022-11-15 15:24:00 +0100

    Move files from local directory

--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/Controller.java
@@ -1,9 +1,65 @@
 package it.unibo.mvc;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.PrintStream;
 
 /**
- * Application controller. Performs the I/O.
+ *
  */
 public class Controller {
+
+    private File currentFile = new File(System.getProperty("user.home") + 
+                                        System.getProperty("file.separator") + 
+                                        "output.txt");
+
+    /**
+     * @return the current file
+     */
+    public File getCurrentFile() {
+        return currentFile;
+    }
+
+    /**
+     * @return the current file path
+     */
+    public String getCurrentFilePath() {
+        return currentFile.getPath();
+    }
+
+    /**
+     * Saves some text on the designed file.
+     * 
+     * @param text
+     *            the text to save
+     * @throws IOException
+     *             if the writing fails
+     */
+    public void save(final String text) throws IOException {
+        try (PrintStream ps = new PrintStream(currentFile)) {
+            ps.println(text);
+        }
+    }
+
+    /**
+     * @param file
+     *            the file where to write
+     */
+    public void setCurrentFile(final File file) {
+        final File parent = file.getParentFile();
+        if (parent.exists()) {
+            currentFile = file;
+        } else {
+            throw new IllegalArgumentException("Cannot save in a non-existing folder.");
+        }
+    }
+
+    /**
+     * @param file
+     *            the file where to write
+     */
+    public void setCurrentFile(final String file) {
+        setCurrentFile(new File(file));
+    }
 
 }
--- 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
+++ 82-simple-mvc-io/src/main/java/it/unibo/mvc/SimpleGUI.java
@@ -1,6 +1,17 @@
 package it.unibo.mvc;
 
+import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.IOException;
+
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
 
 /**
  * A very simple program using a graphical interface.
@@ -8,6 +19,47 @@ import javax.swing.JFrame;
  */
 public final class SimpleGUI {
 
-    private final JFrame frame = new JFrame();
+    private final JFrame frame = new JFrame("Simple GUI");
 
+    private SimpleGUI(final Controller controller) {
+        final JPanel panel1 = new JPanel();
+        panel1.setLayout(new BorderLayout());
+
+        final JTextArea text = new JTextArea();
+        final JButton save = new JButton("Save");
+        save.addActionListener(new ActionListener() {
+            public void actionPerformed(final ActionEvent event) {
+                try {
+                    controller.save(text.getText());
+                } catch (IOException e) {
+                    JOptionPane.showMessageDialog(frame, e, "Error", JOptionPane.ERROR_MESSAGE);
+                }
+            }
+        });
+
+        panel1.add(text, BorderLayout.CENTER);
+        panel1.add(save, BorderLayout.SOUTH);
+        frame.setContentPane(panel1);
+
+        final Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+        final int sw = (int) screen.getWidth();
+        final int sh = (int) screen.getHeight();
+        frame.setSize(sw / 2, sh / 2);
+        frame.setLocationByPlatform(true);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+
+    private void display() {
+        frame.setVisible(true);
+    }
+
+    /**
+     * @param a
+     *            unused
+     */
+    public static void main(final String... a) {
+        final SimpleGUI gui = new SimpleGUI(new Controller());
+        gui.display();
+    }
 }
